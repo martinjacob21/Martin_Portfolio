95fda3a9af621ca85afddaad14ec9869d796c7e9
martinjacob21 <martinjacob2001mj@gmail.com>
2025-04-26 01:48:56 +0530

    Your update message here

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Toaster } from "./components/ui/toaster";
-import React from "react";
 
 createRoot(document.getElementById("root")!).render(
   <>
